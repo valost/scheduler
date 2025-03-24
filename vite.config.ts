f7ef11b852670223d5aabcd9ac8036da9f4dd9df
valost <valerieostrovska@yahoo.com>
2025-03-24 21:46:43 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'scheduler',
+  base: '/scheduler/',
   build: {
     outDir: 'dist'
   }
