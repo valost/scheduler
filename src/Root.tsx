c698137df0b9dcc50d46fe22abda2c949eb4aea7
valost <valerieostrovska@yahoo.com>
2025-03-26 00:01:32 +0200

    added calendar structure

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,20 +2,16 @@ import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { App } from './App';
 import { CalendarPage } from "./modules/CalendarPage/CalendarPage";
 import { HomePage } from "./modules/HomePage/HomePage";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 export const Root = () => (
-  <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />}/>
-          <Route path="calendar" element={<CalendarPage />}/>
-        </Route>
-      </Routes>
-    </Router>
-  </LocalizationProvider>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />}/>
+        <Route path="calendar" element={<CalendarPage />}/>
+      </Route>
+    </Routes>
+  </Router>
 )
 
 export default Root;
--- src/modules/CalendarPage/CalendarPage.tsx
+++ src/modules/CalendarPage/CalendarPage.tsx
@@ -1,10 +1,37 @@
-import { DateCalendar } from '@mui/x-date-pickers';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+// import { getArrowLeftIcon, getArrowRightIcon } from '../../utils/getImages';
 import styles from './CalendarPage.module.scss';
+import { useState } from 'react';
+
+dayjs.locale('uk');
 
 export const CalendarPage = () => {
+  const [currentMonth, setCurrentMonth] = useState(dayjs().startOf('month'));
+  
+  // const daysInMonth = displayed
+
   return (
     <div className={styles.page}>
-      <DateCalendar/>
+      <div className={styles.header}>
+        <Link to="/" className={styles.buttonBack}>Назад</Link>
+
+        <h2 className={styles.title}>Березень 2025</h2>
+
+        <button className={styles.button}>Тиждень</button>
+      </div>
+
+      <div className={styles.weekdays}>
+        <div className={styles.workday}>Пн</div>
+        <div className={styles.workday}>Вт</div>
+        <div className={styles.workday}>Ср</div>
+        <div className={styles.workday}>Чт</div>
+        <div className={styles.workday}>Пт</div>
+        <div className={styles.weekend}>Сб</div>
+        <div className={styles.weekend}>Нд</div>
+      </div>
+
+      <div className={styles.days}></div>
     </div>
   )
 }
