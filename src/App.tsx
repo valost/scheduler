47d5006c88730d5e393c225a4753385fe84ea1e6
valost <valerieostrovska@yahoo.com>
2025-03-26 22:22:46 +0200

    updated main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import styles from './App.module.scss';
+import Routing from './Routing';
 
 export const App: React.FC = () => {
   return (
-    <div className={styles.app}>
-      <Outlet />
-    </div>
+    <Routing />
   )
 }
 
