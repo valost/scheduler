91a87b616c128922b3d136192b6579c086fcdc24
valost <valerieostrovska@yahoo.com>
2025-03-29 21:34:42 +0200

    maciocia 2

--- src/utils/getImages.ts
+++ src/utils/getImages.ts
@@ -1,14 +1,16 @@
 const IMAGE_BASE_PATH = '/img';
 const ICONS_BASE_PATH = '/img/icons';
 
+
+
 const getImages = (name: string): string => {
-  return `${IMAGE_BASE_PATH}/${name}.png`;
+  return `${import.meta.env.BASE_URL}${IMAGE_BASE_PATH}/${name}.png`;
 };
 
 export const getLocationImg = (): string => getImages('locationImg');
 
 const getIcons = (name: string): string => {
-  return `${ICONS_BASE_PATH}/${name}.svg`;
+  return `${import.meta.env.BASE_URL}${ICONS_BASE_PATH}/${name}.svg`;
 }
 
 export const getArrowLeftIcon = (): string => getIcons('arrow-left');
