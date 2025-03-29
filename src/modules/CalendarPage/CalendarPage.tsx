4cc0768354fc008b1798f5ab12827513c986850c
valost <valerieostrovska@yahoo.com>
2025-03-29 19:40:00 +0200

    updated calendar style

--- src/modules/CalendarPage/CalendarPage.tsx
+++ src/modules/CalendarPage/CalendarPage.tsx
@@ -1,13 +1,22 @@
+import styles from './CalendarPage.module.scss';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
-// import { getArrowLeftIcon, getArrowRightIcon } from '../../utils/getImages';
-import styles from './CalendarPage.module.scss';
+import 'dayjs/locale/uk';
 import { useState } from 'react';
+import { getArrowLeftIcon, getArrowRightIcon } from '../../utils/getImages';
+import { bookings } from '../../utils/bookings';
+import { Booking } from '../../types/Booking';
 
 dayjs.locale('uk');
 
 export const CalendarPage = () => {
+  const arrowLeftIcon = getArrowLeftIcon();
+  const arrowRightIcon = getArrowRightIcon();
+  const capitalizeFirstChar = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+
+  const today = dayjs();
   const [currentMonth, setCurrentMonth] = useState(dayjs().startOf('month'));
+  const [selectedDay, setSelectedDay] = useState<number | null>(today.date());
   
   const daysInMonth = currentMonth.daysInMonth();
   const firstDayOfMonth = currentMonth.startOf('month').day();
@@ -17,15 +26,56 @@ export const CalendarPage = () => {
     ...Array(firstDayOffset).fill(null),
     ...daysArray
   ];
+  const formattedDate = capitalizeFirstChar(currentMonth.format('MMMM YYYY'));
+
+  const twoWeeksFromNow = today.add(14, 'day');
+
+  const relevantBookings = bookings.filter((booking) => {
+    const bookingDate = dayjs(booking.startTime);
+
+    return (
+      bookingDate.isSame(today, 'day') ||
+      (bookingDate.isAfter(today) && bookingDate.isBefore(twoWeeksFromNow)) ||
+      bookingDate.isSame(twoWeeksFromNow, 'day')
+    );
+  }).sort((a, b) => dayjs(a.startTime).diff(dayjs(b.startTime)));
+
+  const handlePrevMonthClick = () => {
+    setCurrentMonth(currentMonth.subtract(1, 'month'));
+    setSelectedDay(today.date());
+  }
+
+  const handleNextMonthClick = () => {
+    setCurrentMonth(currentMonth.add(1, 'month'));
+    setSelectedDay(today.date());
+  }
+
+  const handleDayClick = (day: number | null) => {
+    if (day !== null) {
+      setSelectedDay(day === selectedDay ? null : day);
+    }
+  };
 
   return (
     <div className={styles.page}>
       <div className={styles.header}>
-        <Link to="/" className={styles.buttonBack}>Назад</Link>
+        <div className={styles.headerTop}>
+          <Link to="/" className={styles.buttonBack}>Назад</Link>
 
-        <h2 className={styles.title}>{currentMonth.format('MMMM YYYY')}</h2>
+          <button className={styles.button}>Тиждень</button>
+        </div>
 
-        <button className={styles.button}>Тиждень</button>
+        <div className={styles.headerBottom}>
+          <button className={styles.buttonArrow} onClick={handlePrevMonthClick}>
+            <img src={arrowLeftIcon} className={styles.arrow}/>
+          </button>
+
+          <h3 className={styles.title}>{formattedDate}</h3>
+
+          <button className={styles.buttonArrow} onClick={handleNextMonthClick}>
+            <img src={arrowRightIcon} className={styles.arrow}/>
+          </button>
+        </div>
       </div>
 
       <div className={styles.weekdays}>
@@ -39,7 +89,12 @@ export const CalendarPage = () => {
       </div>
 
       <div className={styles.days}>
-        {calendarDays.map((day, index) => (
+        {calendarDays.map((day, index) => {
+          const hasBooking = day && relevantBookings.some(b => 
+            dayjs(b.startTime).date() === day &&
+            dayjs(b.startTime).isSame(currentMonth, 'month')
+          );
+          return (
             <div 
               key={index}
               className={`${styles.day} ${
@@ -49,11 +104,32 @@ export const CalendarPage = () => {
               } ${
                 day === dayjs().date() && currentMonth.isSame(dayjs(), 'month') ? 
                 styles.currentDay : ''
+              } ${hasBooking ? styles.hasBooking : ''} ${
+                day === selectedDay ? styles.selectedDay : ''
               }`}
+              onClick={() => handleDayClick(day)}
             >
               {day || ''}
             </div>
-          ))}
+          );
+        })}
+      </div>
+
+      <h3 className={styles.bookTitle}>Тренування</h3>
+
+      <div className={styles.book}>
+        {bookings.length > 0 ? (
+          <ul className={styles.bookList}>
+            {bookings.map(booking => (
+            <li key={booking.id} className={styles.bookItem}>
+              {dayjs(booking.startTime).format('D MMMM, HH:mm')} - 
+              {dayjs(booking.endTime).format('HH:mm')} - {booking.userName}
+            </li>
+            ))}
+          </ul>
+        ) : (
+          <p>Бронювання відсутні</p>
+        )}
       </div>
     </div>
   )
---
+++ src/utils/bookings.ts
@@ -0,0 +1,44 @@
+export const bookings = [
+  {
+    id: 1,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-27T09:00:00+02:00',
+    endTime: '2025-04-27T11:00:00+02:00',
+  },
+  {
+    id: 2,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-27T12:00:00+02:00',
+    endTime: '2025-04-27T13:00:00+02:00',
+  },
+  {
+    id: 3,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-29T09:00:00+02:00',
+    endTime: '2025-04-29T11:00:00+02:00',
+  },
+  {
+    id: 4,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-14T09:00:00+02:00',
+    endTime: '2025-04-14T11:00:00+02:00',
+  },
+  {
+    id: 5,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-12T09:00:00+02:00',
+    endTime: '2025-04-12T11:00:00+02:00',
+  },
+  {
+    id: 6,
+    userName: 'Random Guy',
+    userId: 1,
+    startTime: '2025-04-26T09:00:00+02:00',
+    endTime: '2025-04-26T11:00:00+02:00',
+  },
+]
