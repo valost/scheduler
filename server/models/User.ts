d312570749cf411a654a75606274fac379aa138f
valost <valerieostrovska@yahoo.com>
2025-05-02 16:25:14 +0300

    added login logic

--- server/models/User.ts
+++ server/models/User.ts
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  repeatPassword: { type: String, required: true },
 });
 
 export const User = mongoose.model('User', userSchema);
--- src/modules/UserPage/UserPage.tsx
+++ src/modules/UserPage/UserPage.tsx
@@ -1,9 +1,17 @@
 // import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styles from './UserPage.module.scss';
+import { useAuth } from '../../context/AuthContext';
 
 export const UserPage = () => {
   // const [bookings, setBookings] = useState([]);
+  const { user, logoutUser } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logoutUser();
+    navigate('/calendar');
+  }
 
   return (
     <div className={styles.page}>
@@ -11,10 +19,14 @@ export const UserPage = () => {
         <Link to="/calendar" className={styles.buttonBack}>
           Назад
         </Link>
+
+        <button onClick={handleLogout} className={styles.buttonExit}>Вихід</button>
       </div>
 
       <div className={styles.wrapper}>
         <h3 className={styles.title}>Мої бронювання</h3>
+
+        <h3>{`Привіт, ${user?.name}!`}</h3>
 
         <table>
           <thead>
